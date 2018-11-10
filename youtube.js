538fba7b4b9f46263cd6cb761d120b095ce8a957
Wyatt Calandro <arm1stice@arm1stice.com>
2018-11-10 15:12:41 -0700

    Add youtube modules

---
+++ youtube.js
@@ -0,0 +1,17 @@
+var validateYoutube = require("youtube-validate");
+var ytdl = require("ytdl-core");
+var path = require("path");
+var fs = require("fs");
+
+module.exports.isValidVideo = function(url){
+  return new Promise(function(resolve, reject){
+    validateYoutube(url).then(resolve).catch(reject);
+  });
+}
+
+module.exports.downloadVideo = function(url){
+  return new Promise(function(resolve, reject){
+    var p = path.join(__dirname, "videos", ytdl.getURLVideoID(url) + ".mp4")
+    ytdl(url).pipe(fs.createWriteStream(p)).on('finish', resolve).on('error', reject);
+  });
+}
