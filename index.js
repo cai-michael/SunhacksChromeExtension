a746912884db3398510d51b243ce4cdc03095f85
Wyatt Calandro <arm1stice@arm1stice.com>
2018-11-10 14:27:29 -0700

    Add comments remove logs

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
+// Import the express module
 var express = require("express");
+// Path module, helps us to construct paths
 var path = require("path");
+// body-parser parses the form bodies so we can access them easily
 var bodyParser = require("body-parser");
+// Check if the url is valid
 var isURL = require("is-url");
+// Allows us to parse url and the query
 var url = require("url");
 var querystring = require("querystring");
 
 var app = express();
 
+// Parse the body of forms
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get("/", function(req, res){
   var p = path.join(__dirname, "views", "home.html");
-  console.log(p)
   res.sendFile(p)
 });
 
@@ -26,7 +31,6 @@ app.get("/hello", function(req, res){
 app.post("/video", function(req, res){
   if(isURL(req.body.videoURL)){
     var parsedURL = url.parse(req.body.videoURL)
-    console.log(parsedURL.host);
     if(!parsedURL.host.includes("youtube.com")){
       res.redirect("/");
     }else{
