0bb734cb96a3580622a6519b116110a6662b21a7
Wyatt Calandro <arm1stice@arm1stice.com>
2018-11-10 14:13:25 -0700

    Implement getting the video ID

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var express = require("express");
 var path = require("path");
+var bodyParser = require("body-parser");
+var isURL = require("is-url");
+var url = require("url");
+var querystring = require("querystring");
 
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get("/", function(req, res){
   var p = path.join(__dirname, "views", "home.html");
@@ -9,8 +15,22 @@ app.get("/", function(req, res){
   res.sendFile(p)
 });
 
+app.get("/videos/:videoid", function(req, res){
+  res.end(req.params.videoid)
+})
+
 app.get("/hello", function(req, res){
   res.end("Hello world!!!")
+});
+
+app.post("/video", function(req, res){
+  if(isURL(req.body.videoURL)){
+    var parsedURL = url.parse(req.body.videoURL)
+    var parsedQuery = querystring.parse(parsedURL.query);
+    res.redirect("/videos/" + parsedQuery.v)
+  }else{
+    res.redirect("/");
+  }
 });
 
 app.listen(8080, function(err){
