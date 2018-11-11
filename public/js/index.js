bb682d31e9358447c5dedec406a6933a8bf5c0b3
Wyatt Calandro <arm1stice@arm1stice.com>
2018-11-10 19:39:27 -0700

    Downloading text

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,18 @@
 var checkloop;
 
 $(document).ready(function () {
-    axios.get("/check/" + vidid).then(function (res) {
+    axios.get("/check/" + vidid, {
+        validateStatus: function (status) {
+            return status < 500; // Reject only if the status code is greater than or equal to 500
+        }
+    }).then(function (res) {
         if (res.data.isValid) {
             if (res.data.downloaded) {
                 $("#videlement").attr("src", "/vids/" + vidid + ".mp4");
                 $("#videlement")[0].load();
             } else {
                 if (res.data.downloading) {
+                    $("#downloadMessage").show();
                     //Show the user that the video is still downloading
                     checkloop = setInterval(checkDownload, 5000); //check every 5 seconds
                 } else {
@@ -18,20 +23,25 @@ $(document).ready(function () {
             alert("Invalid Video");
         }
     })
-    $("[name='Play']").click(function(){
+    $("[name='Play']").click(function () {
         $("#videlement")[0].play();
     })
-    $("[name='Pause']").click(function(){
+    $("[name='Pause']").click(function () {
         $("#videlement")[0].pause();
     })
 })
 
 function checkDownload() {
-    axios.get("/check/" + vidid).then(function (res) {
+    axios.get("/check/" + vidid, {
+        validateStatus: function (status) {
+            return status < 500; // Reject only if the status code is greater than or equal to 500
+        }
+    }).then(function (res) {
         if (res.data.isValid) {
             if (res.data.downloaded) {
                 $("#videlement").attr("src", "/vids/" + vidid + ".mp4");
                 $("#videlement")[0].load();
+                $("#downloadMessage").hide();
                 clearInterval(checkloop);
             }
         }
